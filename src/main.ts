5f8494917d7e05e78ad908836d8b47510c4d0fb3
Simon Lepel <simbo@simbo.de>
2023-09-05 17:42:16 +0200

    optimize assets usage and bundling

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import './style.scss';
 
+import githubBadgeSource from './images/badge-github.svg';
+import spongebobImageSource from './images/mocking-spongebob.jpg';
 import { createImage } from './lib/create-image';
 import { sanitizeText } from './lib/sanitize-text';
 import { updateView } from './lib/update-view';
 import { getURLParameters } from './lib/url-parameters';
 
-const image = await createImage('./mocking-spongebob.jpg');
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <canvas id="canvas" title="right click to copy or save"></canvas>
+  <textarea name="textarea" id="textarea" placeholder="write something…" tabindex="0" cols="30" rows="10"></textarea>
+  <footer>
+    <a href="https://github.com/simbo/mocking-spongebob">
+      <img src="${githubBadgeSource}" alt="simbo/mocking-spongebob" />
+    </a>
+  </footer>
+`;
+
+const image = await createImage(spongebobImageSource);
 const canvas = document.querySelector<HTMLCanvasElement>('#canvas')!;
 const textarea = document.querySelector<HTMLTextAreaElement>('#textarea')!;
 
